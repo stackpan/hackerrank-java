f65d9ea5e8d7048279bd203865e3f9fe56f7f951
ivanzkyanto <ivanrizqy.ir@gmail.com>
2022-10-28 22:25:09 +0700

    Welcome to Java!

---
+++ src/a_introduction/A1_WelcomeToJava.java
@@ -0,0 +1,17 @@
+/**
+ * Challenge name: Welcome to Java!
+ * Challenge URL: https://www.hackerrank.com/challenges/welcome-to-java/problem
+ */
+
+package a_introduction;
+
+public class A1_WelcomeToJava {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* The solution */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        /* End of the solution */
+    }
+}
