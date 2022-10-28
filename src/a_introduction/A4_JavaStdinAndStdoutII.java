741d210dac821262ac3b495370084a252268237a
ivanzkyanto <ivanrizqy.ir@gmail.com>
2022-10-28 22:34:45 +0700

    Java Stdin and Stdout II

---
+++ src/a_introduction/A4_JavaStdinAndStdoutII.java
@@ -0,0 +1,26 @@
+/**
+ * Challenge name: Java Stdin and Stdout II
+ * Challenge URL: https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+ */
+
+package a_introduction;
+
+import java.util.*;
+
+public class A4_JavaStdinAndStdoutII {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* The solution */
+        Scanner scanner = new Scanner(System.in);
+
+        int integerInput = Integer.parseInt(scanner.nextLine());
+        double doubleInput = Double.parseDouble(scanner.nextLine());
+        String stringInput = scanner.nextLine();
+
+        System.out.println("String: " + stringInput);
+        System.out.println("Double: " + doubleInput);
+        System.out.println("Int: " + integerInput);
+        /* End of the solution */
+    }
+}
