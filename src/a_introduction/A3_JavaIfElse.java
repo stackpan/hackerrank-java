745c761948795d978f6c306db449353f34801503
ivanzkyanto <ivanrizqy.ir@gmail.com>
2022-10-28 22:26:34 +0700

    Java If-Else

---
+++ src/a_introduction/A3_JavaIfElse.java
@@ -0,0 +1,29 @@
+/**
+ * Challenge name: Java If-Else
+ * Challenge URL: https://www.hackerrank.com/challenges/java-if-else/problem
+ */
+
+package a_introduction;
+
+import java.io.*;
+
+public class A3_JavaIfElse {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        /* The solution */
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        } else {
+            if (N >= 2 && N <= 5) System.out.println("Not Weird");
+            else if (N >= 6 && N <= 20) System.out.println("Weird");
+            else System.out.println("Not Weird");
+        }
+        /* End of the solution */
+    }
+}
+
